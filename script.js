173e0436c3d2e8319759c956b9903023d68d3fde
Brian Werner <brianmw199@gmail.com>
2023-10-28 05:39:21 -0500

    Set up the newEmployee function and the table is appending

--- script.js
+++ script.js
@@ -12,15 +12,15 @@ let newEmployeeInfo = [];
 function newEmployee(event) {
     event.preventDefault();
     // Testing the function...Hey it works!
-    console.log('Inside the newEmployee function');
+    console.log('Inside the newEmployee function:');
     
     // Putting the newEmployeeInfo inputs into an object
     let newEmployee = {
         firstName: document.getElementById('submitFirstName').value,
         lastName: document.getElementById('submitLastName').value,
         id: document.getElementById('submitId').value,
-        title: document.getElementById('submitTitle'),
-        annualSalary: document.getElementById('submitAnnualSalary')
+        title: document.getElementById('submitTitle').value,
+        annualSalary: document.getElementById('submitAnnualSalary').value
     }
 
     document.getElementById('submitFirstName').value = '';
@@ -28,17 +28,31 @@ function newEmployee(event) {
     document.getElementById('submitId').value = '';
     document.getElementById('submitTitle').value = '';
     document.getElementById('submitAnnualSalary').value = '';
-    
-    console.log(newEmployee);
+
+    console.log('Inside the newEmployee function:', newEmployee);
     newEmployeeInfo.push(newEmployee);
 
-
-
+    // Append items to the DOM
+    let employeeInfo = document.getElementById('employeeInfo');
+    employeeInfo.innerHTML +=  `
     
+    <tr>
+    <td>${newEmployee.firstName}</td>
+    <td>${newEmployee.lastName}</td>
+    <td>${newEmployee.id}</td>
+    <td>${newEmployee.title}</td>
+    <td>${newEmployee.annualSalary}</td>
+    <td><button onclick="deleteInfo(event)">Delete</button></td>
+    </tr>`
+
+    // Testing to see if info appends
+    console.log('Info is appending!!');
 
 };// end of the newEmployee function
 
-// Write a function called annualSalary that should divide totalSalary by a monthlyTotal
+
+
+// Write a function called annualSalary that should divide totalSalary by the monthlyTotal
 // to give us the monthlyTotal
 
 // Write a function called totalCost with a parameter of mnthly
