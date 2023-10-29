5b33889a89aa7c6d454893af465dcd96f9aed897
Brian Werner <brianmw199@gmail.com>
2023-10-29 16:13:35 -0500

    Updated the newEmployee function so that it would calculated the math correctly

--- script.js
+++ script.js
@@ -1,61 +1,87 @@
 console.log('JS loaded');
 
-// Setting two global variables for salary and totalSalary
-let salary = 0;
-let totalSalary = 0;
-
-// Setting a global variable called newEmployeeInfo that is set to an []
-let newEmployeeInfo = [];
+// Setting a global variable for totalMonthly
+let totalMonthly = 0;
 
 // Setting up a function called newEmployee that takes in the value of all the inputs
 // and also will append the employee info to the DOM
 function newEmployee(event) {
     event.preventDefault();
     // Testing the function...Hey it works!
-    console.log('Inside the newEmployee function:');
+    console.log('Testing the newEmployee function:');
     
-    // Putting the newEmployeeInfo inputs into an object
-    let newEmployee = {
-        firstName: document.getElementById('submitFirstName').value,
-        lastName: document.getElementById('submitLastName').value,
-        id: document.getElementById('submitId').value,
-        title: document.getElementById('submitTitle').value,
-        annualSalary: document.getElementById('submitAnnualSalary').value
-    }
+    // Getting the inputs from our ids
+    let firstName = document.getElementById('submitFirstName').value;
+    let lastName = document.getElementById('submitLastName').value;
+    let id = document.getElementById('submitId').value;
+    let title = document.getElementById('submitTitle').value;
+    let annualSalary = document.getElementById('submitAnnualSalary').value;
+    // let total = document.getElementById('total').value;
+    console.log('Test inputs');
 
+
+     // Append items to the DOM
+     document.getElementById('employeeInfo').innerHTML +=  `
+    
+     <tr>
+     <td>${firstName}</td>
+     <td>${lastName}</td>
+     <td>${id}</td>
+     <td>${title}</td>
+     <td>${annualSalary}</td>
+     <td><button onclick="deleteInfo(event)">Delete</button></td>
+     </tr>`
+      
+    
+        
     document.getElementById('submitFirstName').value = '';
     document.getElementById('submitLastName').value = '';
     document.getElementById('submitId').value = '';
     document.getElementById('submitTitle').value = '';
     document.getElementById('submitAnnualSalary').value = '';
 
-    console.log('Inside the newEmployee function:', newEmployee);
-    newEmployeeInfo.push(newEmployee);
-
-    // Append items to the DOM
-    let employeeInfo = document.getElementById('employeeInfo');
-    employeeInfo.innerHTML +=  `
-    
-    <tr>
-    <td>${newEmployee.firstName}</td>
-    <td>${newEmployee.lastName}</td>
-    <td>${newEmployee.id}</td>
-    <td>${newEmployee.title}</td>
-    <td>${newEmployee.annualSalary}</td>
-    <td><button onclick="deleteInfo(event)">Delete</button></td>
-    </tr>`
-
     // Testing to see if info appends
     console.log('Info is appending!!');
+
+    // Incorperating the math logic 
+    totalMonthly += (annualSalary/(12));
+    console.log(totalMonthly);
+    totalMonthly = Number(totalMonthly);
+    totalMonthly = Math.floor(totalMonthly);
+    console.log(totalMonthly);
+
+    console.log('Inside the newEmployee function:');
+
+    
+
+    // Setting up a function that will determine if the monthly total exceeds $20k
+    // it should turn red
+    document.getElementById('total').innerHTML = `Total Monthly: $${totalMonthly}`;
+      if(totalMonthly > 20000) {
+        let footer = document.querySelector('footer');
+        console.log(footer);
+        footer.classList.add('over-budget')
+      }else {
+        let footer = document.querySelector('footer');
+        console.log(footer);
+      }
+
+    
+
+  
+
+    
 
 };// end of the newEmployee function
 
 
 
-// Write a function called annualSalary that should divide totalSalary by the monthlyTotal
-// to give us the monthlyTotal
 
-// Write a function called totalCost with a parameter of mnthly
-// This should also check to see if monthly sales exceeds $20k turn monthly total red, if not keep gray
+
 
 // Write a function called deleteInfo that will delete the rows
+function deleteInfo(event) {
+    console.log('Testing the delete function:');
+    event.target.parentElement.parentElement.remove();
+
+};
